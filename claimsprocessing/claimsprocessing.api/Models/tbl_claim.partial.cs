a2c019cb4e27995ebdb7e8de70066dd5429621c6
Puneeth <10633257+general-coding@users.noreply.github.com>
2025-04-19 15:26:44 +0530

    Yeah, these ideas just mixed my brain. Also, didn't work.

--- claimsprocessing/claimsprocessing.api/Models/tbl_claim.partial.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace claimsprocessing.api.Models
-{
-    [MetadataType(typeof(ClaimMetadata))]
-    public partial class tbl_claim
-    {
-    }
-}
